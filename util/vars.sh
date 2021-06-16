dede5a5081dadbaf2f6d103cd6866ac0616abccb
nihil-admirari <50202386+nihil-admirari@users.noreply.github.com>
2021-06-16 12:38:14 +0300

    Add VP9 timestamp correction patch

--- util/vars.sh
+++ util/vars.sh
@@ -30,7 +30,7 @@ while [[ "$#" -gt 0 ]]; do
     shift
 done
 
-REPO="${GITHUB_REPOSITORY:-btbn/ffmpeg-builds}"
+REPO="${GITHUB_REPOSITORY:-nihil-admirari/ffmpeg-with-vp9-timestamp-fix}"
 REPO="${REPO,,}"
 REGISTRY="ghcr.io"
 BASE_IMAGE="${REGISTRY}/${REPO}/base:latest"
